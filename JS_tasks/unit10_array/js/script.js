598a3200be1db919d9e8493ecef6c74efe532d64
Roma Rub <rubashevskyiroman@gmail.com>
2020-04-08 00:30:02 +0300

    some arraays

--- JS_tasks/unit10_array/js/script.js
+++ JS_tasks/unit10_array/js/script.js
@@ -20,7 +20,7 @@ let b = [4, 2, 5, 23, 43, 1, 2, 6, 12];
 let max = b[0]; //4
 for(let i = 0; i < b.length; i++){
     if(b[i] > max){
-        max =b[i];
+        max = b[i];
     }
 }
 console.log('max:' + max)
@@ -44,4 +44,54 @@ b1.onclick = function () {
     }
     document.querySelector('.out2').innerHTML = out1;
 }
+//==============================
+let styles = ['jazz', 'bluzz'];
+styles.push('rock-n-roll');
+styles[1] = 'classic';
+//console.log(styles.shift());
+styles.unshift('rap', 'reggy');
+console.log(styles);
 
+//======================
+function sumInput() {
+
+    let numbers = [];
+    while (true) {
+
+    let value = prompt("Введите число", 0);
+
+      // ends enter values
+    if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+    }
+
+    let sum = 0;
+    for (let number of numbers) {
+    sum += number;
+    }
+    return sum;
+}
+
+//console.log( sumInput() );
+
+//===========================
+function getMaxSubSum(arr) {
+    let maxSum = 0;
+    let partialSum = 0;
+
+    for (let item of arr) { // для каждого элемента массива
+      partialSum += item; // добавляем значение элемента к partialSum
+      maxSum = Math.max(maxSum, partialSum); // запоминаем максимум на данный момент
+      if (partialSum < 0) partialSum = 0; // ноль если отрицательное
+    }
+
+    return maxSum;
+}
+
+  console.log( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+  console.log( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+  console.log( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+  console.log( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+  console.log( getMaxSubSum([1, 2, 3]) ); // 6
+  console.log( getMaxSubSum([-1, -2, -3]) ); // 0

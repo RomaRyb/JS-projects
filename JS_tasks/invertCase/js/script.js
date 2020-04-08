068faa92e1d1b7f7a672a4def226490078ac769a
Roma Rub <rubashevskyiroman@gmail.com>
2020-04-08 17:28:08 +0300

    solwe with ? operator

--- JS_tasks/invertCase/js/script.js
+++ JS_tasks/invertCase/js/script.js
@@ -1,15 +1,25 @@
+// let invertCase = (str) => {
+//     let result = "";
+
+//     for(let i = 0; i < str.length; i++){
+        
+//         if (str[i] === str[i].toUpperCase()){
+//             result += str[i].toLowerCase();
+//         } if (str[i] === str[i].toLowerCase()){
+//             result += str[i].toUpperCase();
+//         }
+//     }
+//     return result;
+// }
+// console.log(invertCase('I leARn JaVScripT'));
+
+
 let invertCase = (str) => {
     let result = "";
 
-    for(let i = 0; i < str.length; i++){
-        
-        if (str[i] === str[i].toUpperCase()){
-            result += str[i].toLowerCase();
-        } if (str[i] === str[i].toLowerCase()){
-            result += str[i].toUpperCase();
-        }
+    for(let i = 0; i < str.length; i++){        
+        result = (str[i] === str[i].toUpperCase()) ? (result += str[i].toLowerCase()) : (result += str[i].toUpperCase());
     }
     return result;
 }
 console.log(invertCase('I leARn JaVScripT'));
-

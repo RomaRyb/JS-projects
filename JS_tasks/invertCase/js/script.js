1fa7c7b9c0b263f7948e4225e14b2dc84ef0ec62
Roma Rub <rubashevskyiroman@gmail.com>
2020-04-13 23:34:57 +0300

    added

--- JS_tasks/invertCase/js/script.js
+++ JS_tasks/invertCase/js/script.js
@@ -75,3 +75,41 @@ const diff = (a, b) => {
 console.log(diff(0,45));
 console.log(diff(0,190));
 console.log(diff(180,180));
+
+// ==============================================================
+//Напишите функцию sumSquareDifference, которая принимает аргумент n и возвращает разницу между квадратом суммы и суммой квадратов первых n натуральных чисел.
+//Сумма квадратов первых десяти натуральных чисел это 12 + 22 + 32 + ... + 10 2 = 385 (
+//Квадрат суммы первых десяти натуральных чисел это (1 + 2 + 3 + ... + 10)2 = 552 = 3025.
+//Разница между квадратом суммы и суммой квадратов первых десяти натуральных чисел: 3025 − 385 = 2640.
+
+const sumSquareDifference = n => {
+    let sum1 = 0;
+    for(let i = 0; i <= n; i++){
+        sum1 += i ** 2;
+    }
+    let sum2 = 0;
+    for (let i = 0; i <= n; i++){
+        sum2 += i;
+    }
+    return (sum2 ** 2) - sum1;
+}
+
+console.log(sumSquareDifference(1));
+console.log(sumSquareDifference(3));
+console.log(sumSquareDifference(8));
+
+// ==============================================================
+
+
+export default (str) => {
+    let acc = 0;
+    for (let i = 0; i < str.length; i++){
+        const symbol = str[i];
+        acc = symbol === '(' ? acc + 1 : acc - 1;
+        if(acc < 0) {
+            return false;
+        }
+    }
+    return acc === 0;
+};
+
